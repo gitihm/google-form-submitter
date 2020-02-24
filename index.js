566b595afa764602b7807d1b65f1ab8ed6db51d4
air <s5935512030@phuket.psu.ac.th>
2020-02-24 20:14:49 +0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let timer = null;
 const rand = () => Math.floor(Math.random() * 10) + 1;
 const post = () =>
   request.post(
-    "https://docs.google.com/forms/u/0/d/e/1FAIpQLSe4RCHjykHRmTV--t31qLkSoqTuWIwge2D_Z2bsdlkkelZ4ng/formResponse",
+    "https://docs.google.com/forms/d/1gTWCRK4phPKtxYvxxS_ma_h_AO_BtcKlAHqdxVQey5U/formResponse",
     {
       headers: {
         accept:
@@ -25,31 +25,13 @@ const post = () =>
         "x-client-data":
           "CIu2yQEIprbJAQjBtskBCKmdygEI4qjKAQjKr8oBCM6wygEIq7LKAQ==",
         referer:
-          "https://docs.google.com/forms/u/0/d/e/1FAIpQLSe4RCHjykHRmTV--t31qLkSoqTuWIwge2D_Z2bsdlkkelZ4ng/formResponse"
+          "https://docs.google.com/forms/d/1gTWCRK4phPKtxYvxxS_ma_h_AO_BtcKlAHqdxVQey5U/formResponse"
       },
-      form: encodeURI(`entry.1124049050=${
-        rand() > 5 ? "Female" : "Male"
-      }&entry.335377238=${
-        rand() > 8 ? "Less than 23" : rand() > 5 ? "31 to 50" : "more than 50"
-      }&entry.1167156427=${
-        rand() > 8
-          ? "With friends"
-          : rand() > 5
-          ? "With family (sons, relatives, etc)"
-          : "Boyfriend/Girlfriend"
-      }&entry.915067496=${
-        rand() > 8
-          ? "Monthly"
-          : rand() > 7
-          ? "Once a year"
-          : rand() > 5
-          ? "Twice a year"
-          : "More than 5 times a year"
-      }&fvv=1&draftResponse=[[[null,1038019991,["${
-        rand() > 8 ? "Community" : rand() > 5 ? "Adventure" : "Sight-seeing"
+      
+      form: encodeURI(`fvv=1&draftResponse=[[[null,1212348438,["${
+        rand() > 8 ? "XS" : rand() > 5 ? "S" : "M"
       }"]
-    ,0]
-    ,[null,2012118827,["${
+    ,0],[null,865000962,["${
       rand() > 7
         ? "Center"
         : rand() > 7
@@ -59,57 +41,9 @@ const post = () =>
         : "Northeast"
     }"]
     ,0]
-    ,[null,1118193831,["${
-      rand() > 5
-        ? "Following someone else’s plan (tour guide, online plan, etc)"
-        : "Planning before leaving home"
-    }"]
-    ,0]
-    ,[null,163025962,["${
-      rand() > 7
-        ? "Internet websites"
-        : rand() > 7
-        ? "Asking the locals of your destination"
-        : "Asking service providers, such as hotels clerks or tourist guides"
-    }"]
-    ,0]
-    ,[null,1993826339,["${
-      rand() > 7
-        ? "3,000฿-4,000฿ per person"
-        : rand() > 7
-        ? "5,000฿-7,000฿ per person"
-        : rand() > 7
-        ? "8,000฿-10,000฿ per person"
-        : "More than 10,000฿ per person"
-    }"]
-    ,0]
-    ,[null,1075586648,["${
-      rand() > 7
-        ? "half day trip"
-        : rand() > 7
-        ? "one day trip"
-        : rand() > 7
-        ? "2 days 1 night"
-        : rand() > 5
-        ? "3 days 2 nights"
-        : "1 week"
-    }"]
-    ,0]
-    ,[null,1626751957,["${
-      rand() > 7
-        ? "Hotel"
-        : rand() > 7
-        ? "Resort"
-        : rand() > 7
-        ? "Villa"
-        : "Homestay"
-    }"]
-    ,0]
-    ,[null,1886407717,["${rand() > 7 ? "Yes" : rand() > 5 ? "No" : "Maybe"}"]
-    ,0]
   ]
-    ,null,"-2897307624769073063"]
-    &pageHistory=0,1,2,3,4&fbzx=-2897307624769073063`)
+    ,null,"29752272310871733"]
+    &pageHistory=0&fbzx=29752272310871733`)
     },
     (e, r, b) => {
       console.log(++i, b.indexOf("เราได้บันทึกคำตอบของคุณไว้แล้ว") !== -1);
@@ -118,5 +52,5 @@ const post = () =>
   );
 
 post();
-timer = setInterval(post, 300);
-setTimeout(() => clearInterval(timer), 60000);
+timer = setInterval(post, 100);
+// setTimeout(() => clearInterval(timer), 60000);
